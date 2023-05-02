698ebd187a4f2e5bb617f0c45868d0205651b7ae
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-05-02 11:14:05 +0600

    designed the individual chef cards in a component using the fetched data

--- src/HomeComponents/Chef.jsx
+++ src/HomeComponents/Chef.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Chef = ({ chef }) => {
-    
+    const { chef_code, imageChef, chefName, experience, likes, recipeNumber } = chef;
+
+    const handleViewRecipe = (code) => {
+        console.log(code)
+    }
+    return (
+        <div className='p-6 border rounded-lg shadow-md text-gray-500'>
+            <img src={imageChef} alt="chef image" className='rounded-lg mb-8 w-full h-[350px] object-cover' />
+
+            <div className='flex flex-col gap-1 mb-8'>
+                <p >Name : {chefName}</p>
+                <p>Experience: {experience} years</p>
+                <p>Likes : {likes}</p>
+                <p>Number of recipes : {recipeNumber}</p>
+            </div>
+            <button className='px-6 py-2 bg-slate-800 text-yellow-200 font-bold rounded-lg' onClick={() => handleViewRecipe(chef_code)}>View Recipes</button>
+        </div>
     );
 };
 
