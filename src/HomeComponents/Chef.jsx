f369aa2f3c4de782cb4348010fe48bcc68b0a111
aaachris1820 <63766879+aaachris1820@users.noreply.github.com>
2023-05-02 11:12:36 +0600

    designed the chefs section for showcasing chef cards

---
+++ src/HomeComponents/Chef.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Chef = ({ chef }) => {
+    
+    );
+};
+
+export default Chef;
--- src/HomeComponents/Home.jsx
+++ src/HomeComponents/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import BannerHome from './BannerHome'
+import Chefs from './Chefs';
 
 const Home = () => {
     return (
         <main>
             <BannerHome></BannerHome>
+            <Chefs></Chefs>
         </main>
     );
 };
